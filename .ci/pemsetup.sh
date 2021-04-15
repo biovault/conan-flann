92a53479e204437028151c10dae2dc9e2cb85ee7
B. van Lew <b.van_lew@lumc.nl>
2021-04-15 16:05:31 +0200

    Fixup cacert for lkeb-artifactory access

---
+++ .ci/pemsetup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo Extend conan cacert.pem
+if [ $# -eq 0 ] ; then
+    conanhome=`conan config home`
+else
+    conanhome=$1
+fi
+echo Conan home: $conanhome/cacert.pem
+echo Lines in cacert.pem before update: $(wc -l $conanhome/cacert.pem)
+cat cacert.pem >> $conanhome/cacert.pem
+echo Lines in cacert.pem after update : $(wc -l $conanhome/cacert.pem)
