5423a346fc6dccba47de0532c63cf2a266030030
Baldur van Lew <baldurvanlew@gmail.com>
2021-05-20 17:45:30 +0200

    Set for 1.9.1 dual build (Win and Mac)

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class FlannDualConan(ConanFile):
     name = "flann"
-    version = "1.8.4"
+    version = "1.9.1"
     license = "MIT"
     author = "B. van Lew b.van_lew@lumc.nl"
     url = "https://dl.bintray.com/bldrvnlw/conan-repo/flann"
@@ -33,14 +33,15 @@ class FlannDualConan(ConanFile):
                 "flann/src/cpp/CMakeLists.txt",
                 'add_library(flann SHARED "")',
                 'add_library(flann SHARED "empty.cpp")')
-        if self.settings.os == "Macos":
-            tools.replace_in_file(
-                "flann/src/cpp/flann/algorithms/kdtree_index.h",
-                "#include <cstring>", '''#include <cstring>
-#include <cmath>''')
-            tools.replace_in_file(
-                "flann/src/cpp/flann/algorithms/kdtree_index.h",
-                "abs", "std::fabs")
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        conanproj = ("project(flann)\n" 
+                "include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n"
+                "conan_basic_setup()"
+        ) 
+        tools.replace_in_file("flann/CMakeLists.txt", "project(flann)", conanproj)
+
 
     def _configure_cmake(self, build_type):
         if self.settings.os == "Macos":
