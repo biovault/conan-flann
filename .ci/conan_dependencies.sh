f029fd0863d1b3fd04b00275b755879337acea71
bldrvnlw <b.van_lew@lumc.nl>
2021-06-28 16:54:43 +0200

    Add Linux build

--- .ci/conan_dependencies.sh
+++ .ci/conan_dependencies.sh
@@ -9,12 +9,14 @@ cd $project && .ci/pemsetup.sh
 conan remote add lkeb-artifactory $CONAN_UPLOAD
 conan remote list
 
-if [[ $TRAVIS_OS_NAME -eq osx ]] ; then
+if [[ $TRAVIS_OS_NAME == osx ]] ; then
     CONAN_OS=Macos
     CONAN_COMPILER=apple-clang
+    CONAN_CPP_LIB=libc++
 else
     CONAN_OS=Linux
     CONAN_COMPILER=gcc
+    CONAN_CPP_LIB=libstdc++
 fi
 
 conan profile new action_build --force
@@ -24,5 +26,5 @@ conan profile update settings.arch=x86_64 action_build
 conan profile update settings.arch_build=x86_64 action_build
 conan profile update settings.compiler=$CONAN_COMPILER action_build
 conan profile update settings.compiler.version=$CONAN_COMPILERVERSION action_build
-conan profile update settings.compiler.libcxx=libc++ action_build
+conan profile update settings.compiler.libcxx=$CONAN_CPP_LIB action_build
 conan profile show action_build
