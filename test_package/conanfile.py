96b6e30ad32c7ced13ec5a25ef599e9d456b01bd
bldrvnlw <b.van_lew@lumc.nl>
2021-07-30 15:26:00 +0200

    Add test_package

---
+++ test_package/conanfile.py
@@ -0,0 +1,45 @@
+import os
+import h5py
+import numpy as np
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+
+
+class FlannTestConan(ConanFile):
+    name = "FlannTest"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+    requires = ("hdf5/1.10.6", "lz4/1.9.2")
+    exports = "CMakeLists.txt", "example.cpp"
+
+    def generate(self):
+        print("Generating toolchain")
+        tc = CMakeToolchain(self)
+        tc.variables["flann_ROOT"] = self.deps_cpp_info["flann"].rootpath
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    # def imports(self):
+    #    self.copy("*.dll", dst="bin", src="bin")
+    #    self.copy("*.dylib*", dst="bin", src="lib")
+    #    self.copy("*.so*", dst="bin", src="lib")
+    #    self.copy("flann", dst="flann", folder=True)
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            with h5py.File("dataset.hdf5", "w") as f:
+                np_d = np.random.randint(0, 128, size=(9000, 128), dtype=np.uint8)
+                np_d = np_d.astype(np.float64)
+                np_q = np.random.randint(0, 128, size=(1000, 128), dtype=np.uint8)
+                np_q = np_q.astype(np.float64)
+                f.create_dataset("dataset", data=np_d)
+                f.create_dataset("query", data=np_q)
+            print("Running example...")
+            # tools.download("http://www.cs.ubc.ca/research/flann/uploads/FLANN/datasets/dataset.hdf5", "dataset.hdf5")
+            self.run(".%sexample" % os.sep)
