8733e2d4347c832b422e33109b94cb6e199f9783
Baldur van Lew <b.van_lew@lumc.nl>
2024-05-14 13:56:57 +0200

    [skip ci] restore conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -85,10 +85,6 @@ message(STATUS "OpenMP library: $<$<LINK_LANGUAGE:CXX>:${OpenMP_CXX_LIBRARIES}> 
     def requirements(self):
         self.requires.add("lz4/1.9.2")
 
-    def compatibility(self):
-        if self.settings.compiler == "apple-clang":
-            return [{"settings": [("compiler.version", "13")]}]
-
     def _get_tc(self):
         """Generate the CMake configuration using
         multi-config generators on all platforms, as follows:
